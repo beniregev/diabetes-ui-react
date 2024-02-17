ecf2db3b1bcf12ccd199832a841c757ec37a75e5
binyamin.regev <beniregev@gmail.com>
2024-02-18 00:56:12 +0200

    Adding routes for NoMatch and Data Tables pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/home/Home";
 import { Add } from "./pages/add/Add";
 import { Bulk } from "./pages/bulk/Bulk";
-import { Home } from "./pages/home/Home";
+import { DataTables } from "./pages/data-tables/DataTables";
 import { About } from "./pages/about/About";
 import { DataReceivedSuccessfully } from "./pages/DataReceivedSuccessfully";
+import { NoMatch } from "./pages/NoMatch";
 import "./App.css";
 
 function App() {
@@ -21,6 +23,9 @@ function App() {
         <Route path="/bulk" element={<Bulk />}>
           Bulk
         </Route>
+        <Route path="/dt" element={<DataTables />}>
+          Data Tables
+        </Route>
         <Route path="/about" element={<About />}>
           About
         </Route>
@@ -32,6 +37,7 @@ function App() {
           path="/bulk/data-received"
           element={<DataReceivedSuccessfully />}
         />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   );
