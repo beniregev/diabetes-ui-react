cd42e7e8bfa540fe064a469169059a3d019ec976
binyamin.regev <beniregev@gmail.com>
2024-02-18 00:05:46 +0200

    Adding routing to Add, Bulk, and DataReceivedSuccessfully pages for navigating programmatically

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { Add } from "./pages/add/Add";
+import { Bulk } from "./pages/bulk/Bulk";
 import { Home } from "./pages/home/Home";
 import { About } from "./pages/about/About";
+import { DataReceivedSuccessfully } from "./pages/DataReceivedSuccessfully";
 import "./App.css";
 
 function App() {
@@ -12,9 +15,23 @@ function App() {
         <Route path="/" element={<Home />}>
           Home
         </Route>
+        <Route path="/add" element={<Add />}>
+          Add
+        </Route>
+        <Route path="/bulk" element={<Bulk />}>
+          Bulk
+        </Route>
         <Route path="/about" element={<About />}>
           About
         </Route>
+        <Route
+          path="/add/data-received"
+          element={<DataReceivedSuccessfully />}
+        />
+        <Route
+          path="/bulk/data-received"
+          element={<DataReceivedSuccessfully />}
+        />
       </Routes>
     </>
   );
